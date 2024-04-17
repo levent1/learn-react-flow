0e6bccd2a196e569e117344f08a39da790a6f551
levent <tarik.kyncu@gmail.com>
2024-04-17 15:13:43 +0300

    m

--- src/pages/Flow.jsx
+++ src/pages/Flow.jsx
@@ -1,4 +1,4 @@
-import  { useCallback, useState ,useEffect} from 'react';
+import  { useCallback, useState ,useEffect, useRef} from 'react';
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -30,7 +30,9 @@ export default function App() {
   const [nodeName, setNodeName] = useState('');
   const [infoBlockVisible, setInfoBlockVisible] = useState(false);
   const [taskDescription, setTaskDescription] = useState('');
- 
+  const infoBlockRef = useRef(null);
+
+
  const onConnect = useCallback(
   (connection) => {
     setEdges((oldEdges) => addEdge(connection, oldEdges));
@@ -70,7 +72,9 @@ const handleEdgeClick = (event, edge) => {
 
 const handeleNodeDoubleClick =(event,node)=>{
   console.log('Tıklanan Çift Tıklanan Düğüm:', node);
+  setSelectedNode(node);
   setInfoBlockVisible(true);
+  setNodeName(node.data.label);
 };
 
 const deleteNode = useCallback(() => {
@@ -99,80 +103,91 @@ const handeleChangeDropdown = (event) => {
   setSelectedOption(event.target.value);
 };
 
-const handleNodeNameChange = (event) => {
-  setNodeName(event.target.value);
+const handleTaskDescriptionChange = (event) => {
+  const newDescription = event.target.value;
+  setTaskDescription(newDescription);
+  updateNodeInfo(nodeName, selectedOption, newDescription); // Node ismi, seçilen opsiyon ve görev açıklamasını güncelle
 };
 
 const handleSelectedOptionChange = (event) => {
-  setSelectedOption(event.target.value);
+  const newOption = event.target.value;
+  setSelectedOption(newOption);
+  updateNodeInfo(nodeName, newOption, taskDescription); // Node ismi, seçilen opsiyon ve görev açıklamasını güncelle
 };
 
-const handleTaskDescriptionChange = (event) => {
-  setTaskDescription(event.target.value);
+const handleNodeNameChange = (event) => {
+  const newName = event.target.value;
+  setNodeName(newName);
+  updateNodeInfo(newName, selectedOption, taskDescription); // Node ismi, seçilen opsiyon ve görev açıklamasını güncelle
 };
 
-const handleOutsideClick = () => {
-  if (infoBlockVisible) {
-    updateNodeInfo();
-    setInfoBlockVisible(false);
+
+const handleOutsideClick = (event) => {
+  if (infoBlockRef.current && !infoBlockRef.current.contains(event.target)) {
+    if (infoBlockVisible) {
+      updateNodeInfo(nodeName,selectedOption,taskDescription); // Node ismini güncelle
+      setInfoBlockVisible(false);
+    }
   }
 };
 
-const updateNodeInfo = () => {
-  const updatedNodes = nodes.map(n => {
-    if (n.id === selectedNode.id) {
-      return {
-        ...n,
-        data: {
-          label: nodeName,
-          unit: selectedOption,
-          toDo: taskDescription
-        }
-      };
-    }
-    return n;
-  });
-  setNodes(updatedNodes);
+const updateNodeInfo = (newName, newOption, newDescription) => {
+  if (selectedNode) { // selectedNode null değilse devam et
+    const updatedNodes = nodes.map(n => {
+      if (n.id === selectedNode.id) {
+        return {
+          ...n,
+          data: {
+            ...n.data,
+            label: newName,
+            unit: newOption, // selectedOption değerini güncelle
+            toDo: newDescription // taskDescription değerini güncelle
+          }
+        };
+      }
+      return n;
+    });
+    setNodes(updatedNodes);
+  }
 };
   
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <div className='tools'>
-        <button className='btn' onClick={addNode}>Node Ekle</button>
-        <button className='btn' onClick={deleteNode}>Sil</button>
-        <button className='btn'>Kaydet</button>
-       <select className='dropdown' value={selectedOption} onChange={handeleChangeDropdown}>
-        <option value="İzin Talebi">İzin Talebi</option>
-        <option value="Eğitim Talebi"> Eğitim Talebi</option>
-        <option value="Avans Talebi"> Avans Talebi</option>
-        <option value="Harcama Talebi">Harcama Talebi</option>
-       </select>
-
-        
-      </div>
-      
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        elements={[]} 
-        onNodeClick={(event, node) => handleNodeClick(event, node)}
-        onEdgeClick={(event, edge) => handleEdgeClick(event, edge)}
-        onNodeDoubleClick={(event,node)=>handeleNodeDoubleClick(event,node)}
-        >
-          <input></input>
-        <div style={{ width: '100vw', height: '100vh' }} onClick={handleOutsideClick}>
-      {infoBlockVisible && (
-        <div className='infoblock'>
-          <input
-            type="text"
-            value={nodeName}
-            placeholder='Noda isim ver'
-            onChange={handleNodeNameChange}
-          />
-          <select
+return (
+  <div style={{ width: '100vw', height: '100vh' }} onClick={handleOutsideClick}>
+    <div className='tools'>
+      <button className='btn' onClick={addNode}>Node Ekle</button>
+      <button className='btn' onClick={deleteNode}>Sil</button>
+      <button className='btn'>Kaydet</button>
+     <select className='dropdown' value={selectedOption} onChange={handeleChangeDropdown}>
+      <option value="İzin Talebi">İzin Talebi</option>
+      <option value="Eğitim Talebi"> Eğitim Talebi</option>
+      <option value="Avans Talebi"> Avans Talebi</option>
+      <option value="Harcama Talebi">Harcama Talebi</option>
+     </select>
+    </div>
+    
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      elements={[]} 
+      onNodeClick={(event, node) => handleNodeClick(event, node)}
+      onEdgeClick={(event, edge) => handleEdgeClick(event, edge)}
+      onNodeDoubleClick={(event,node)=>handeleNodeDoubleClick(event,node)}
+    >
+      <div style={{ position: 'relative' }}>
+        {nodes.map(node => (
+          <div key={node.id} style={{ position: 'absolute', left: node.position.x + node.width / 2, top: node.position.y + node.height / 2}}>
+            {node.id === selectedNode?.id && infoBlockVisible && (
+              <div className='infoblock' style={{ position: 'absolute', top: 50, left: '100%', marginLeft: 10 }} ref={infoBlockRef}>
+                <input
+                  type="text"
+                  value={nodeName}
+                  onChange={handleNodeNameChange}
+                  placeholder='Noda isim ver'
+                />
+                <select
             className='active_unit'
             value={selectedOption}
             onChange={handleSelectedOptionChange}
@@ -187,15 +202,18 @@ const updateNodeInfo = () => {
             value={taskDescription}
             onChange={handleTaskDescriptionChange}
           ></textarea>
-         </div>  
-         )}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
 
-       </div>
-        <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
-        <NodeToolbar />
-      </ReactFlow>
-    </div>
-  );
+      <Controls />
+      <MiniMap />
+      <Background variant="dots" gap={12} size={1} />
+      <NodeToolbar />
+    </ReactFlow>
+  </div>
+);
+
 }
