d2199eea081ffdedd568ac641451253acb54923e
levent <tarik.kyncu@gmail.com>
2024-04-22 14:01:19 +0300

    Ust bar sürükleme islemi yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home.jsx'; // Doğru dosya yolunu kullanın
 
 import Flow from './pages/Flow.jsx';
 import PersonelRequest from './pages/PersonelRequest.jsx';
+import Shape from './pages/DnDFlow.jsx';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="flow" element={<Flow/>} />
         <Route path='requestpersonal' element={<PersonelRequest/>}/>
+        <Route path='shape' element={<Shape/>}/>
+
       </Routes>
     </BrowserRouter>
   );
